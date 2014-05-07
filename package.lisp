ce63098050201022b35b2df35084e0821e38dec1
Shinmera <shinmera@tymoon.eu>
2014-05-07 11:19:25 +0200

    Added exports

--- package.lisp
+++ package.lisp
@@ -6,4 +6,18 @@
 
 (defpackage wavesim
   (:nicknames #:org.tymoonnext.kiga.wavesim)
-  (:use #:cl))
+  (:use #:cl)
+  ;; simulator.lisp
+  (:export
+   #:wave-environment
+   #:current
+   #:advance
+   #:start-wave)
+  ;; main.lisp
+  (:export
+   #:*thread*
+   #:*window-size*
+   #:*granularity*
+   #:*spacing*
+   #:start
+   #:start-threaded))
